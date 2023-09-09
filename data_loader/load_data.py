563e659f695cf682994d5399ede8cfaf08499345
Burak Kaya <brk.kya34@yahoo.com>
2023-09-09 16:30:18 +0300

    initial commit for repository

---
+++ data_loader/load_data.py
@@ -0,0 +1,4 @@
+from langchain.document_loaders import UnstructuredPDFLoader
+
+def load_pdf_file():
+    pass
---
+++ utils/utils.py
@@ -0,0 +1,9 @@
+def generate_summary_prompt(example):
+    """Generates a standardized message to prompt the model with an instruction, optional input and a
+    'output' field."""
+    return f"### Instruction:\n{example['instruction']}\n\n### Response:\n"
+
+def generate_explain_prompt(example):
+    """Generates a standardized message to prompt the model with an instruction, optional input and a
+    'output' field."""
+    return f"### Instruction:\n{example['instruction']}\n\n### Response:\n"
