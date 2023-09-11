d5af135a4438815380d467e50ee7f21d3e5f0f83
Burak Kaya <brk.kya34@yahoo.com>
2023-09-11 17:37:22 +0300

    add ctransformer model

--- model_engine/load_text_encoder.py
+++ model_engine/load_text_encoder.py
@@ -1,19 +1,18 @@
-from transformers import LlamaForCausalLM, LlamaTokenizer, BitsAndBytesConfig
-import torch
-
+from ctransformers import AutoModelForCausalLM, AutoConfig
+# from utils.utils import generate_explain_prompt, generate_summary_prompt 
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
 
 class TextEncoder:
-    def __init__(self) -> None:
+    def __init__(self, model_name_or_path: str, model_type: str) -> None:
+        self.model_config = AutoConfig.from_pretrained(model_name_or_path)
+        self.model = AutoModelForCausalLM.from_pretrained(model_name_or_path, model_type=model_type)
+
+    def generate_summary(self, prompt: str):
+
+        self.model.generate()
         pass
-
-
-# #
-# model_id = "TheBloke/Llama-2-13B-fp16"
-
-
-# bnb_config = BitsAndBytesConfig(
-#     load_in_4bit=True,
-# )
-
-# LlamaTokenizer.from_pretrained(model_id)
-# LlamaForCausalLM.from_pretrained(model_id, quantization_config=bnb_config, device_map="auto")
+# llm = AutoModelForCausalLM.from_pretrained("TheBloke/Luban-13B-GGUF",hf=True, model_file="luban-13b.q4_K_M.gguf",)
