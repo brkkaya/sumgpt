eb8679a3a2352abda8f4ed64b467b4a681b12434
Burak Kaya <brk.kya34@yahoo.com>
2023-09-13 16:17:40 +0300

    testing prompts

--- chat_engine/summarize_doc.py
+++ chat_engine/summarize_doc.py
@@ -1,8 +1,11 @@
 from typing import List
+from langchain import PromptTemplate
 from langchain.docstore.document import Document
 from ctransformers import AutoConfig
 from langchain.llms import CTransformers
 from langchain.chains.summarize import load_summarize_chain
+from langchain.chains import LLMChain
+from utils.utils import summary_instruction, explanation_instruction
 
 
 class SummarizeDoc:
@@ -11,19 +14,30 @@ class SummarizeDoc:
         self.model = model
         self.embedding_history = []
         self.summarize_history = []
-        self.map_reduce_chain = load_summarize_chain(llm=model, chain_type="map_reduce")
+        self.map_reduce_chain = load_summarize_chain(
+            llm=model,
+            chain_type="map_reduce",
+        )
         self.refine = load_summarize_chain(llm=model, chain_type="refine")
+
+    def summarizer_per_doc(self):
+        PROMPT = PromptTemplate(template=summary_instruction, input_variables=["text"])
+
+        summary_chain = LLMChain(llm=self.model, prompt=PROMPT)
+        output = summary_chain(self.documents)
+        return output
 
 
 from data_loader.load_data import DataLoading
+
 docs = DataLoading().load_pdf_file("./data/2309.00071.pdf")
-model_config = AutoConfig.from_pretrained("luban-13b.Q4_K_M.gguf")
+model_config = AutoConfig.from_pretrained("model/luban-13b.Q4_K_M.gguf")
 model_config.model_type = "llama"
-model = CTransformers(
-            model="luban-13b.Q4_K_M.gguf", configs=model_config
-        )
-
-summary = SummarizeDoc(documents=docs,model=model)
-refine_summary = summary.refine(docs)
+model_config.config.context_length = 4096
+model = CTransformers(model="model/luban-13b.Q4_K_M.gguf", configs=model_config)
+PROMPT = PromptTemplate(template=summary_instruction, input_variables=["text"])
+model.stream(PROMPT)
+model.generate(PROMPT)
+summary = SummarizeDoc(documents=docs[0], model=model)
+refine_summary = summary.summarizer_per_doc()
 print(refine_summary)
-
--- utils/utils.py
+++ utils/utils.py
@@ -1,29 +1,16 @@
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 
-explanation_instruction = SystemMessage(
-    content=(
-        "You are a helpful, respectful and honest assistant with a deep knowledge of code and software design. Always"
-        " answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical,"
-        " racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased"
-        " and positive in nature.If a question does not make any sense, or is not factually coherent, explain why"
-        " instead of answering something not correct. If you don't know the answer to a question, please don't share"
-        " false information.\nI want you to act as a simple explanation provider for difficult concepts. I will provide"
-        " a brief description of a concept, and you will respond with a clear and concise explanation in layman’s"
-        " terms. Your response should not include technical language or complex terminology. Instead, you must focus on"
-        " breaking down the concept into easy-to-understand language."
-    )
+explanation_instruction = (
+    "### Instruction:\n\nI want you to act as a simple explanation provider for difficult concepts. I will provide a brief"
+    " description of a concept, and you will respond with a clear and concise explanation in layman’s terms. Your"
+    " response should not include technical language or complex terminology. Instead, you must focus on breaking down"
+    " the concept into easy-to-understand language. Explain text below:\n{text}\n\n### Response: "
 )
 
-summary_instruction = SystemMessage(
-    content=(
-        "You are a helpful, respectful and honest assistant with a deep knowledge of code and software design. Always"
-        " answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical,"
-        " racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased"
-        " and positive in nature.If a question does not make any sense, or is not factually coherent, explain why"
-        " instead of answering something not correct. If you don't know the answer to a question, please don't share"
-        " false information.\nI want you to act as a text summarizer to help me create a concise summary of the text I"
-        " provide. The summary cannot be long but it has to expressing the key points and concepts written in the"
-        " original text without adding your interpretations. Please ensure that your summary is clear, concise, and"
-        " accurately reflects the content. "
-    )
+
+summary_instruction = (
+    "### Instruction:\n\nI want you to act as a text summarizer to help me create a concise summary of the text I"
+    " provide. The summary cannot be long but it has to expressing the key points and concepts written in the"
+    " original text without adding your interpretations. Please ensure that your summary is clear, concise, and"
+    " accurately reflects the content. Explain text below:\n{text}\n\n### Response: "
 )
