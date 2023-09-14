8799da1333e2df3979144d3e54393fe3f015989d
Burak Kaya <brk.kya34@yahoo.com>
2023-09-14 16:09:27 +0300

    summary looks enough but, explain not work correctly

--- model_engine/load_text_encoder.py
+++ model_engine/load_text_encoder.py
@@ -10,15 +10,17 @@ class TextEncoder:
         self.model = AutoModelForCausalLM.from_pretrained(
             model_name_or_path, model_type=model_type, config=self.model_config
         )
-        
 
     def generate_summary(self, prompt: str, is_stream: bool = True):
-        for word in self.model(prompt=prompt, stream=is_stream):
+        for word in self.model(
+            prompt=prompt, stream=is_stream, max_new_tokens=2048, stop=["### Instruction", "### Response", "###", "###"]
+        ):
             print(word, end="", flush=True)
         pass
 
     def generate_embedding(self, prompt: str):
         return self.model.embed(prompt)
+
 
 d: Document = DataLoading().load_pdf_file("data/2309.00071.pdf")
 
@@ -26,14 +28,24 @@ llm = TextEncoder(
     "model/luban-13b.Q4_K_M.gguf",
     model_type="llama",
 )
+for docs in d:
+    # summary_instruction = (
+    #     "### Instruction:\n\nI want you to act as a text summarizer to help me create a concise summary of the text I"
+    #     " provide. The summary cannot be long but it has to expressing the key points and concepts written in the"
+    #     " original text without adding your interpretations. Please ensure that your summary is clear, concise, and"
+    #     f" accurately reflects the content. Explain text below:\n{docs.page_content}\n\n### Response:"
+    # )
+    explanation_instruction = (
+        "### Instruction:\n\nI want you to act as a Mathematician and have a PhD in CS. You will explain difficult"
+        " concepts. I will provide you article based on mathematics and computer science , and you will respond with a"
+        " clear and concise explanation. Your response should include technical language and complex terminology. You"
+        " need to explain every formula."
+        "You will extract the key-concepts and explain their mathematical formulas."
+        f" Explain the context in heavily to fill every gap of context. Explain text below:\n{docs.page_content}\n\n###"
+        " Response: \n"
+    )
 
-summary_instruction = (
-    "### Instruction:\n\nI want you to act as a text summarizer to help me create a concise summary of the text I"
-    " provide. The summary cannot be long but it has to expressing the key points and concepts written in the"
-    " original text without adding your interpretations. Please ensure that your summary is clear, concise, and"
-    f" accurately reflects the content. Explain text below:\n{d[0].page_content}\n\n### Response: "
-)
-
-# llm.generate_summary("What is the meaning of life?")
-print(summary_instruction)
-llm.generate_summary(summary_instruction)
+    # llm.generate_summary("What is the meaning of life?")
+    print(explanation_instruction)
+    llm.generate_summary(explanation_instruction)
+    print("\n\n")
